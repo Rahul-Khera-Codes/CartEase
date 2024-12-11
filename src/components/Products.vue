2b5b2b7d73eb169f9cd810f6458d14851bff9cb7
Rahul-Khera-Codes <rahul.excel2011@gmail.com>
2024-12-11 16:25:12 +0100

    ensured everything is in sync with the latest edits

---
+++ src/components/Products.vue
@@ -0,0 +1,200 @@
+``<template>
+<div class="product effect2">
+  <div class="card">
+    <div class="card-image">
+      <figure class="image is-1by1">
+        <img class="image-proper" :src= "product.picture" alt="Placeholder image"/>
+        <img class="brand-overlay" :src="brandOverlay" alt="Placeholder image"/>
+      </figure>
+    </div>
+  <div class="card-content">
+    <p class="title is-4">{{product.name}}</p>
+    <div class="media">
+      <div class="media-left">
+        <SelectionOrder v-bind:product="product" v-on:size-select="selectPrice" v-bind:orderprice="select"  v-bind:orderpricelast="lastSelectedPrice" v-bind:orderpromo="currentPromo"/>
+      <div class="media-content">
+      </div>
+        <p class="subtitle is-6">Ціна: <b>{{select}}</b> грн
+          <i v-if="currentPromo" class="promo-true">{{lastSelectedPrice}} грн</i>
+          <i v-else class="promo-false"></i>
+      </p>
+      <ol>
+        <li v-if="currentPromo" class="is-6"><b class="sale">Знижка!</b>
+        <li v-else> <b class="is-6">Без знижки</b></li>
+        <li v-if="checkHappyHours()"><b class="is-6 sale">{{superSale.description}} <br/> {{superSale.time_start}}:00 - {{superSale.time_end}}:00 <br/> {{superSale.sale}} % на одну<br/> за рандомом.</b></li>
+        <li> <b class="is-6 sale">При перевищенні {{superSale.price_over}} грн,</b> <br/> <p class="sale">безкоштовна доставка</p></li>
+      </ol>
+      </div>
+    </div>
+
+    <div class="content"> <b>Інгредієнти: </b>
+      {{product.description}}
+      <br>
+      <i>Дата та час: </i>
+      <time datetime="product.time">{{product.time}}</time>
+    </div>
+  </div>
+</div>
+  <!-- <h3>{{product.name}}</h3>
+  <div class="image">
+    <img class="chart-icon" :src="chartIcon" alt="">
+    <img class="image" :src="image" alt="">
+    <img class="brand-overlay" :src="brandOverlay" alt="">
+  </div>
+  <div>
+    <b>Price:{{product.currentprice}}</b>
+    <b v-if="product.promo" class="promo-false" v-bind:class="{'promo-true':product.promo}">
+      {{product.lastprice}}
+    </b>
+  </div>
+  <div class="description">
+    {{product.description}}
+  </div> -->
+</div>
+</template>
+
+<script>
+import brandOverlay from '../assets/overlay.png'
+import SelectionOrder from '../components/SelectionOrder.vue'
+
+export default {
+  name: 'Product',
+  components: {
+    SelectionOrder
+  },
+  props: ['product'],
+  methods: {
+    selectPrice (size) {
+      console.log(size)
+      switch (size) {
+        case '24cm':
+          this.select = this.product.currentprice.s
+          this.lastSelectedPrice = this.product.lastprice.s
+          this.currentPromo = this.product.promo.s
+          break
+        case '30cm':
+          this.select = this.product.currentprice.m
+          this.lastSelectedPrice = this.product.lastprice.m
+          this.currentPromo = this.product.promo.m
+          break
+        case '35cm':
+          this.select = this.product.currentprice.xl
+          this.lastSelectedPrice = this.product.lastprice.xl
+          this.currentPromo = this.product.promo.xl
+          break
+        default:
+          this.select = this.product.currentprice.xl
+          this.lastSelectedPrice = this.product.lastprice.xl
+          this.currentPromo = this.product.promo.xl
+      }
+    },
+    checkHappyHours () {
+      const globalDate = new Date()
+      const getDayOfWeek = globalDate.getDay()
+      const getHours = globalDate.getHours()
+      const destructionDoW = this.superSale.dayofweekpromo.split('')
+      console.log(getDayOfWeek)
+      console.log(getHours)
+      console.log(parseInt(this.superSale.time_start))
+      console.log(destructionDoW)
+      if (getDayOfWeek >= parseInt(destructionDoW[0]) && getDayOfWeek <= parseInt(destructionDoW[1])) {
+        if (getHours >= parseInt(this.superSale.time_start) && getHours <= parseInt(this.superSale.time_end)) {
+          this.$store.commit('dailyPromo', true)
+          return true
+        } else {
+          return false
+        }
+      }
+    }
+  },
+  msg: {
+    type: String,
+    required: false
+  },
+  data: function () {
+    return {
+      brandOverlay: brandOverlay,
+      select: this.product.currentprice.xl,
+      lastSelectedPrice: this.product.lastprice.xl,
+      currentPromo: this.product.promo.xl,
+      superSale: this.$store.getters.sale[0],
+      activeDailyPromo: this.$store.getters.dailypromo
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.image{
+}
+.image-proper{
+}
+.extra-paddings{
+  padding-top: 3px;
+  padding-bottom: 3px;
+}
+.extra-paddings-button-chart{
+  margin-left: 20px;
+}
+.brand-overlay {
+  position: absolute!important;
+  top:80%!important;
+  left:60%!important;
+  bottom: 0!important;
+  height:40px!important;
+  width: 120px!important;
+  opacity: 0.6;
+  filter: alpha(opacity=60);
+  /* For IE8 and earlier */
+    border-radius: 5 px;
+}
+.promo-true {
+  text-decoration: line-through;
+  color: #cc0000;
+}
+.promo-false{
+  color: #ffffff;
+}
+.sale {
+  color: #cc0000;
+}
+.product{
+  z-index:0!important;
+}
+.effect2
+{
+  position: relative!important;
+}
+.effect2:before, .effect2:after
+{
+  z-index: -1;
+  position: absolute;
+  content: "";
+  bottom: 12px;
+  left: 6px;
+  width: 47%;
+  top: 96%;
+  max-width:300px;
+  background: #777!important;
+  -webkit-box-shadow: 0 15px 10px #777;
+  -moz-box-shadow: 0 15px 10px #777;
+  box-shadow: 0 15px 10px #777;
+  -webkit-transform: rotate(-3deg);
+  -moz-transform: rotate(-3deg);
+  -o-transform: rotate(-3deg);
+  -ms-transform: rotate(-3deg);
+  transform: rotate(-3deg);
+}
+.effect2:after
+{
+  z-index: -1;
+  -webkit-transform: rotate(3deg);
+  -moz-transform: rotate(3deg);
+  -o-transform: rotate(3deg);
+  -ms-transform: rotate(3deg);
+  transform: rotate(3deg);
+  right: 10px;
+  left: auto;
+}
+
+</style>
---
+++ src/components/SelectionOrder.vue
@@ -0,0 +1,131 @@
+<template>
+  <section>
+    <div class="block">
+     <b-radio @input="$emit('size-select',radio)" class="extra-paddings" v-bind:class="{'radio-active':radio==='35cm' }" v-model="radio"
+     name="name"
+     type="is-danger"
+     native-value="35cm">
+     XL
+    </b-radio>
+    <b-radio @input="$emit('size-select',radio)" class="extra-paddings" v-bind:class="{'radio-active':radio==='30cm' }" v-model="radio"
+     name="name"
+     type="is-success"
+     native-value="30cm">
+     M
+     </b-radio>
+     <b-radio @input="$emit('size-select',radio)" class="extra-paddings" v-bind:class="{'radio-active':radio==='24cm' }" v-model="radio"
+     name="name"
+     native-value="24cm">
+     S
+     </b-radio>
+       <b-button v-if="orderprice>0" class="extra-paddings-button-chart" @click="addProduct(product)" type="is-primary" outlined pack="fas" icon-left="shopping-cart"></b-button>
+       <b-button v-else disabled class="extra-paddings-button-chart" type="is-primary" outlined pack="fas" icon-left="shopping-cart"></b-button>
+     </div>
+     <p class="content">
+       <b>Розмір піцци:</b>
+     {{ radio }}
+     </p>
+  </section>
+</template>
+
+<script>
+import uuid from 'uuid'
+export default {
+  name: 'SelectionOrder',
+  methods: {
+    snackbar () {
+      this.$buefy.snackbar.open(`Default, positioned isbottom with a green 'OK' button`)
+    },
+    addProduct (product) {
+      // console.log('-------------PRODUCT--------------')
+      // console.log(product)
+      // console.log('-------------PRODUCT--------------')
+      this.$buefy.snackbar.open({
+        message: 'Додано до кошика',
+        position: 'is-bottom',
+        actionText: 'Переглянути покупки',
+        onAction: () => {
+          this.$buefy.toast.open({
+            message: 'Action pressed',
+            queue: false
+          })
+          this.$router.push({ path: 'chart' })
+        }
+      })
+      var today = new Date()
+      // var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
+      // var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
+      // var dateTime = date + ' ' + time
+      // Recreate new object, othervise we change original object
+      const uuidRecord = uuid()
+
+      const newProduct = {
+        currentprice: this.orderprice,
+        lastprice: this.orderpricelast,
+        description: product.description,
+        icon: product.icon,
+        id: uuidRecord,
+        name: product.name,
+        picture: product.picture,
+        promo: this.orderpromo,
+        quantity: product.quantity,
+        size: this.radio,
+        date: today
+      }
+      this.$store.commit('addToChart', newProduct)
+      // -> 1addToChart
+    }
+  },
+  props: {
+    product: {
+      product: []
+    },
+    orderprice: {
+      type: Number,
+      required: false
+    },
+    orderpricelast: {
+      type: Number,
+      required: false
+    },
+    orderpromo: {
+      type: Boolean,
+      required: false
+    }
+  },
+  components: {},
+  data: function () {
+    return {
+      radio: '35cm'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+// *{
+//   outline: 1px dotted red;
+// }
+.icon-button{
+  margin:0px;
+  padding:0px;
+  height: 44px;
+  width: 44px;
+  // outline: 1px dotted red;
+}
+.extra-paddings{
+  padding-top: 12px;
+  padding-bottom: 12px;
+  height: 45px;
+  width: 60px;
+}
+.extra-paddings-button-chart{
+  margin-left: 20px;
+  height: 44px;
+  width: 44px;
+}
+.radio-active{
+  border: 1px solid #9400ff;
+  border-radius: 5px;
+}
+</style>
