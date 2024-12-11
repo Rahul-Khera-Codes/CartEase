b0576fd23f9a3176dc55d7780eba53d879492bcf
Rahul-Khera-Codes <rahul.excel2011@gmail.com>
2024-12-11 23:55:16 +0100

    polished up some code and made improvements

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <Main v-bind:products="products" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Main from '@/components/Main.vue'
+
+export default {
+  name: 'Home',
+  props: {
+    msg: {
+      type: String,
+      required: false
+    },
+    products: []
+  },
+  components: {
+    Main
+  }
+}
+</script>
