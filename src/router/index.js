f1b342663cae51e7fc64be711447bf4b82ca0f4d
Rahul-Khera-Codes <rahul.excel2011@gmail.com>
2024-12-12 02:25:17 +0100

    ensured everything is in sync with the latest edits

---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/chart',
+    name: 'chart',
+    // route level code-splitting
+    // this generates a separate chunk (chart.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "chart" */ '../views/Chart.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    // route level code-splitting
+    // this generates a separate chunk (chart.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "chart" */ '../views/Login.vue')
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    // route level code-splitting
+    // this generates a separate chunk (chart.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "chart" */ '../views/Signup.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/views/About.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="about">
+  <section class="section">
+    <div class="container">
+      <h1 class="title">О проекте</h1>
+      <h2 class="subtitle">
+        Дякуемо <strong>opensource</strong> розробникам за цей проект.
+      </h2>
+    </div>
+  </section>
+  <div class="tile is-ancestor">
+  <div class="tile is-vertical is-8">
+    <div class="tile">
+      <div class="tile is-parent is-vertical">
+        <article class="tile is-child notification is-primary">
+          <p class="title">Хактоберфест 2019</p>
+          <p><a href="https://github.com/CapitanFindusFI">CapitanFindusFI</a></p>
+          <p><a href="https://github.com/coffeebeanspoem">coffeebeanspoem</a></p>
+          <p><a href="https://github.com/DaveSalazar">DaveSalazar</a></p>
+        </article>
+        <article class="tile is-child notification is-warning">
+          <p class="title">...також</p>
+          <p><a href="https://github.com/ewatch">ewatch</a></p>
+          <p><a href="https://github.com/frahmadani">frahmadani</a></p>
+          <p><a href="https://github.com/harshalkh">Harshal</a></p>
+        </article>
+      </div>
+      <div class="tile is-parent">
+        <article class="tile is-child notification is-info">
+          <p class="title">Панда Біла Церква</p>
+          <p class="subtitle">Піцца Панда БЦ</p>
+          <figure class="image is-4by3 centered">
+            <img src="../assets/map.png"/>
+          </figure>
+          Б.Церква, вул. Героев Чернобыля 5а
+        </article>
+      </div>
+    </div>
+    <div class="tile is-parent">
+      <article class="tile is-child notification is-danger">
+        <p class="title">gavelWeb</p>
+        <p class="subtitle">and Brotherhood of Eden</p>
+        <div class="content">
+          <p>Цей проект був неможливий без VUE команди, компанії Digital Ocean, Github та Microsoft. Спеціальні подяки до Kotis Design.</p>
+        </div>
+      </article>
+    </div>
+  </div>
+  <div class="tile is-parent">
+    <article class="tile is-child notification is-success">
+      <div class="content">
+        <p class="title">І набагато більше...</p>
+        <p class="subtitle">від Хактоберфесту</p>
+        <div class="content">
+          <p><a href="https://github.com/henrycunh">Henrique Cunha</a></p>
+          <p><a href="https://github.com/Joel-hanson">Joel Hanson</a></p>
+          <p><a href="https://github.com/pablostellet">Pablo Stellet Dias</a></p>
+          <p><a href="https://github.com/ravecampus">Lavwin C. Campollo</a></p>
+          <p><a href="https://github.com/syndu77">syndu77</a></p>
+          <p><a href="https://github.com/ulfayanti">ulfayanti</a></p>
+        </div>
+      </div>
+    </article>
+  </div>
+</div>
+  </div>
+</template>
